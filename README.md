cd09541764d5dce19f12a453c0c7a3d52d032bc5
Vitalii Timoshenko <gametime1@yandex.ru>
2025-02-26 09:50:09 +0600

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Veribrary API
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'User 1',
+            'email' => '1@api.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'User 2',
+            'email' => '2@api.com',
+        ]);
+    }
+}
